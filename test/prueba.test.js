cc4dc6a40d7fe06ad59e1c5fba2fdf24d8ff0707
kdiazc1 <kdiazc@unal.edu.co>
2024-05-01 19:23:24 -0500

    feat(): corrección de ci/cd

--- test/prueba.test.js
+++ test/prueba.test.js
@@ -1,7 +1,19 @@
 const request = require('supertest');
+const nock = require('nock');
 const app = require('../index'); 
 
 describe('Pruebas de integración básicas', () => {
+  beforeAll(() => {
+    // Mocking the external services
+    nock('https://appointments-gogtlsicya-uk.a.run.app')
+      .get('/appointment')
+      .reply(200, 'Appointment service response');
+
+    nock('http://35.221.2.215:3002')
+      .get('/users')
+      .reply(200, 'Users service response');
+  });
+
   it('Debe responder con código 200 en la ruta de prueba', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
